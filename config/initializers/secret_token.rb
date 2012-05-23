361b7bf5236ddbbfd74423ade5264dfdd4f507fc
Daniel Chen <daniel85.chen@gmail.com>
2012-05-23 18:09:13 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wheretoeatca::Application.config.secret_token = '4b4e12b244073da1d77b97aa437eaff5d7f555aea5b6a0f6d5424a7e64eb6c5030e27274840e3e8c3200e3dbdf24bdf7888eaa55d57722f4f27b4a8803539e11'
